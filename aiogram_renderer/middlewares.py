d68a78e3786b70909d206e22c0922b87d1e6936b
m-xim <170838360+m-xim@users.noreply.github.com>
2025-05-30 16:33:22 +0300

    refactor: simplify get fsm in middleware

--- aiogram_renderer/middlewares.py
+++ aiogram_renderer/middlewares.py
@@ -1,7 +1,9 @@
-from aiogram import types
-from aiogram import BaseMiddleware
 from typing import Any, Callable
+
+from aiogram import BaseMiddleware
 from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
+
 from .bot_mode import BotModes, BotMode
 from .renderer import Renderer
 from .window import Window
@@ -12,16 +14,13 @@ class RendererMiddleware(BaseMiddleware):
         self.windows = windows
         self.modes = modes
 
-    async def __call__(self, handler: Callable, event: types.Message, data: dict[str, Any]) -> Any:
-        # Если есть FSMContext то передаем его в renderer и bot_modes
-        for key, value in data.items():
-            if isinstance(value, FSMContext):
-                bot_modes = BotModes(*self.modes, fsm=value) if (self.modes is not None) else None
-                renderer = Renderer(bot=event.bot, fsm=value, windows=self.windows, bot_modes=bot_modes)
-                data["renderer"] = renderer
-                result = await handler(event, data)
-                del renderer
-                return result
+    async def __call__(self, handler: Callable, event: Message, data: dict[str, Any]) -> Any:
+        # Если есть FSMContext, то передаем его в renderer и bot_modes
+        if (fsm := data.get('state')) is not None:
+            data["renderer"] = Renderer(
+                bot=event.bot,
+                fsm=fsm,
+                windows=self.windows,
+                bot_modes=BotModes(*self.modes, fsm=fsm) if (self.modes is not None) else None)
 
-        result = await handler(event, data)
-        return result
+        return await handler(event, data)
